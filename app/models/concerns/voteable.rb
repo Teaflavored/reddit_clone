d19c9d7a2a41e56c3555bb6b8273233086f76142
Auster Chen <auster.s.chen@gmail.com>
2014-10-31 17:18:50 -0700

    minor refactoring

--- app/models/concerns/voteable.rb
+++ app/models/concerns/voteable.rb
@@ -2,13 +2,33 @@ module Voteable
   extend ActiveSupport::Concern
   
   included do 
-    has_many :votes, as: :voteable
-    
-    def self.order_by_score
-      self.select("#{self.to_s.downcase.pluralize}.*, COALESCE(SUM(votes.value), 0) AS sum")
-      .joins("LEFT OUTER JOIN votes ON votes.voteable_id = #{self.to_s.downcase.pluralize}.id AND votes.voteable_type = '#{self}'")
-      .group("#{self.to_s.downcase.pluralize}.id").order("sum DESC, #{self.to_s.downcase.pluralize}.created_at DESC")
-    end
-    
+    #all this stuff run from class scope!
+    has_many :votes, as: :voteable  
+  end
+
+  #methods at the root here will become instance methods
+  def sing
+    puts "I LOVE VOTING LALALALA"
+  end
+  
+  #these become class methods
+  module ClassMethods
+    def order_by_score
+      self.select(<<-SQL)
+        #{self.to_s.downcase.pluralize}.*, COALESCE(SUM(votes.value), 0) 
+      AS 
+        sum
+      SQL
+      .joins(<<-SQL)
+      LEFT OUTER JOIN 
+        votes 
+      ON 
+        votes.voteable_id = #{self.to_s.downcase.pluralize}.id 
+      AND 
+        votes.voteable_type = '#{self}'
+      SQL
+      .group("#{self.to_s.downcase.pluralize}.id")
+      .order("sum DESC, #{self.to_s.downcase.pluralize}.created_at DESC")
+    end    
   end
 end
